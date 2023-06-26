939417c85e833fcb2dcf36881a9a961bb57d1fde
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-06-26 14:39:09 +0300

    edited: register model Post

--- apps/social/admin.py
+++ apps/social/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.social.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "owner", "created", "updated", "total_likes")
+    filter_horizontal = ("user_likes",)

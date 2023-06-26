4d07078d50ecf0d193bfc906dc25decb6052baf4
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-06-26 14:42:48 +0300

    edited: create model Post

--- apps/social/models.py
+++ apps/social/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    user_likes = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="images_liked",
+        blank=True,
+    )
+    total_likes = models.IntegerField(db_index=True, default=0, blank=True)
